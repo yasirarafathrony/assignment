c8364e77616a0eb56f0263d6db3ee9c25d68ca2c
Yasir Arafath <rony1382006@gmail.com>
2021-08-04 23:30:40 +0300

    Adding Docker part

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
-const debug = require('debug')('weathermap');
+/* eslint-disable comma-dangle */
+// const debug = require('debug')('weathermap');
+require('dotenv').config();
 
 const Koa = require('koa');
 const router = require('koa-router')();
@@ -6,8 +8,8 @@ const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -19,7 +21,7 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
